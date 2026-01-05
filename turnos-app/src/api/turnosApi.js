7e9aaea2c279dcebbb4742d8b941c2da027f96be
leandroArias1 <Wazzybtw@gmail.com>
2026-01-05 13:47:54 -0300

    m

--- turnos-app/src/api/turnosApi.js
+++ turnos-app/src/api/turnosApi.js
@@ -1,5 +1,6 @@
 const API_URL = import.meta.env.VITE_API_URL || "http://localhost:4000";
 
+
 export async function getTurnos() {
   const res = await fetch(`${API_URL}/api/turnos`);
   const data = await res.json().catch(() => null);
